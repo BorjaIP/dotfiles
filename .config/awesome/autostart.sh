1d4f83dd4ad89b33f9ba6e94cbd5329c199397ba
BorjaIP <borja.irpe@gmail.com>
2020-04-18 13:24:54 +0000

    autostart script

---
+++ .config/awesome/autostart.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+## run (only once) processes which spawn with the same name
+function run {
+   if (command -v $1 && ! pgrep $1); then
+     $@&
+   fi
+}
+
+## run (only once) processes which spawn with different name
+if (command -v gnome-keyring-daemon && ! pgrep gnome-keyring-d); then
+    gnome-keyring-daemon --daemonize --login &
+fi
+if (command -v start-pulseaudio-x11 && ! pgrep pulseaudio); then
+    start-pulseaudio-x11 &
+fi
+if (command -v /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 && ! pgrep polkit-mate-aut) ; then
+    /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
+fi
+if (command -v  xfce4-power-manager && ! pgrep xfce4-power-man) ; then
+    xfce4-power-manager &
+fi
+if (command -v system-config-printer-applet && ! pgrep applet.py ); then
+  system-config-printer-applet &
+fi
+
+# Network monitor
+run nm-applet
+# Blur and transparency
+run compton
+# Thunar run faster
+run thunar --daemon
+run xfsettingsd
+# Bluetooth control
+# run blueman-applet
+# run pa-applet
+# Notifications
+# run msm_notifier
+
